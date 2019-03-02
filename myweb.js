55f3c1ec4a974d58827424d1d9103a3670624665
unknown <nkchia@cs.washington.edu>
2019-03-01 19:22:56 -0800

    Tidied the overall project

--- myweb.js
+++ myweb.js
@@ -1,58 +1,65 @@
+"use strict";
+
 let displayTimer;
 let phrases = ["Simplistic Style.",
     "Interested in NLP.",
     "Sophomore.",
     "From Malaysia."];
 
-$(document).ready(function(){
+(function() {
+  window.onload = function() {
     setup();
-});
+  };
 
-function setup() {
-  waitDisplay();
-  $("#expButton").click(setExp);
-}
-
-function waitDisplay() {
-  displayTimer = setTimeout(reduceText, 3000);
-}
-
-function reduceText(){
-  displayTimer = setInterval(reduceStep, 70);
-}
-
-function reduceStep() {
-  let changeLength = $("#display p").html().length;
-  if (changeLength <= 1) {
-    clearInterval(displayTimer);
-    increaseText();
-  } else {
-    $("#display p").html($("#display p").html().substring(0, changeLength - 2) + "|");
-  }
-}
-
-function increaseText(){
-  let index = Math.floor(Math.random() * phrases.length);
-  let targetPhrase = phrases[index];
-
-  displayTimer = setInterval(increaseStep, 70, targetPhrase);
-}
-
-function increaseStep(targetPhrase) {
-  let changeLength = $("#display p").html().length - 1;
-  if (targetPhrase.length == changeLength) {
-    clearInterval(displayTimer);
+  function setup() {
     waitDisplay();
-  } else {
-    $("#display p").html(targetPhrase.substring(0, changeLength + 1) + "|");
   }
-}
 
-function setExp() {
-  $("#experience").removeClass("hidden");
-  $("#experience div:first").addClass("visible");
-  $("#experience").one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',
-    function(e) {
+  function waitDisplay() {
+    displayTimer = setTimeout(reduceText, 3000);
+  }
 
-  });
-}
+  function reduceText(){
+    displayTimer = setInterval(reduceStep, 70);
+  }
+
+  function increaseText(){
+    let index = Math.floor(Math.random() * phrases.length);
+    let targetPhrase = phrases[index];
+
+    displayTimer = setInterval(increaseStep, 70, targetPhrase);
+  }
+
+  function reduceStep() {
+    let changeLength = qs("#display p").innerHTML.length;
+    if (changeLength <= 1) {
+      clearInterval(displayTimer);
+      increaseText();
+    } else {
+      qs("#display p").innerHTML = qs("#display p").innerHTML.substring(0, changeLength - 2) + "|";
+    }
+  }
+
+  function increaseStep(targetPhrase) {
+    let changeLength = qs("#display p").innerHTML.length - 1;
+    if (targetPhrase.length == changeLength) {
+      clearInterval(displayTimer);
+      waitDisplay();
+    } else {
+      qs("#display p").innerHTML = targetPhrase.substring(0, changeLength + 1) + "|";
+    }
+  }
+
+  //Helper Functions
+  function $(id) {
+    return document.getElementById(id);
+  }
+
+  function qs(selector) {
+    return document.querySelector(selector);
+  }
+
+  function qsa(selector) {
+    return document.querySelectorAll(selector);
+  }
+})();
